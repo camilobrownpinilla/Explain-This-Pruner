88827a0bcb16aa389a85f5776c94473a056edbd0
camilobrownpinilla <camilobrownpinilla@gmail.com>
2024-10-18 14:10:42 -0400

    Codebase skeleton.

---
+++ evaluations/evaluations.py
@@ -0,0 +1,14 @@
+
+
+class Evaluator():
+    """
+    Wrapper to allow for easy evaluation of pruning methods
+    """
+    def __init__(self): # TODO Discuss what would make sense. maybe init with eval metric?
+        pass
+
+    def evaluate(self, args): 
+        """
+        Method to evaluate a pruning method.
+        """
+        pass
---
+++ pruners/pruners.py
@@ -0,0 +1,19 @@
+
+
+class Pruner():
+    """
+    Wrapper around pruning methods, allowing easy access
+    """
+    def __init__(self, model):
+        pass
+
+    def prune(model, method):
+        """
+        Prune a model with a given method
+
+        Params:
+            model: Model to be pruned.
+            method: Pruning method to use.
+        """
+        pass
+        
