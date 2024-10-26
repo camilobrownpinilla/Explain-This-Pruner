61be0c85029e935d767fe41934b3c9a323cd127b
camilobrownpinilla <camilobrownpinilla@gmail.com>
2024-10-26 16:44:15 -0400

    Refactor and generalize code to extract model parameters

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 
 def get_device():
@@ -8,3 +9,19 @@ def get_device():
         return torch.device("cuda")
     else:
         return torch.device("cpu")
+    
+def get_params_to_prune(model):
+    """Returns all non-embedding parameters of a PyTorch model for pruning.
+
+    Args:
+        model: Model whose parameters are to be pruned
+
+    Returns:
+        List: List of parameters to be passed to a PyTorch pruning method
+    """
+    model_params = []
+    for name, module in model.named_modules():
+        if not isinstance(module, nn.Embedding):  # Skip embedding layers
+            if hasattr(module, 'weight'):  # Ensure it has a weight parameter
+                model_params.append((module, 'weight'))
+    return model_params
