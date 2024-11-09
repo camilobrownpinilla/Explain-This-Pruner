12f83644c5ea62b74eddb9f8a0b6b245f3ee7129
Joey Bejjani <jpbejjani@hotmail.com>
2024-11-08 19:22:39 -0500

    implement local infidelity via pair masking; refactor; get MASK id from tokenizer

--- evaluations/evaluator.py
+++ evaluations/evaluator.py
@@ -1,8 +1,9 @@
-import numpy as np
 import torch
-from explainers.explainer import Explainer
-
+from itertools import combinations
 from transformers import AutoTokenizer, BertForSequenceClassification
+from copy import deepcopy
+
+from explainers.explainer import Explainer
 from explainers.explanation_methods import SHAP, IG
 from utils.utils import get_device
 
@@ -11,7 +12,6 @@ class Evaluator():
     """
     Wrapper for evaluation of explanations
     """
-    MASK = 103  # [MASK] token maps to 103
 
     # TODO Discuss what would make sense. maybe init with eval metric?
     def __init__(self, explainer: Explainer):
@@ -19,8 +19,11 @@ class Evaluator():
         self.tokenizer = explainer.tokenizer
         self.explainer = explainer  # explanation method to evaluate
         self.device = explainer.device
+        # explanation method used to produce `explanation`
+        self.method = type(self.explainer).__name__
+        self.MASK = tokenizer.mask_token_id  # id of [MASK] token
 
-    def evaluate_infidelity_mask_top_k(self, test_set, k=1, num_samples=None):
+    def evaluate_infidelity_mask_top_k(self, test_set, k=1, ptg=0.2):
         """
         Returns the average local infidelity of the explanation method over `num_samples` rand samples in `test_set`.
         Computes local infidelity by masking top k features of an input sample.
@@ -28,18 +31,16 @@ class Evaluator():
         params:
             test_set: a `Dataset` object, e.g. obtained via `load_dataset("imdb")["test"]`
             k: number of top features to mask
-            num_samples: number of test samples to evaluate on
+            ptg: percentage of test set to evaluate on. default 20%
 
         returns:
             average infidelity
         """
-        # evaluate on full test set by default
-        if num_samples is None:
-            num_samples = len(test_set)
 
         # shuffle test set for random sampling
         shuffled_set = test_set.shuffle(seed=4)  # set seed for reproducibility
         infid = 0
+        num_samples = int(len(shuffled_set) * ptg)
         for sample in shuffled_set[:num_samples]:
             infid += self.get_local_infidelity_mask_top_k(sample["text"], k)
 
@@ -66,51 +67,38 @@ class Evaluator():
         predicted_class_id = logits.argmax().item()
         explanation = self.explainer.explain(input)
 
-        top_feature_ids, top_features = self.get_top_k_features(
-            explanation, type(self.explainer).__name__, predicted_class_id, k)
+        top_feature_ids = self.get_top_k_features(
+            explanation, predicted_class_id, k)
 
-        # perturb input by replacing top k features with MASK token `103`
-        tokens = tokenized_input["input_ids"]
-        tokens[0, top_feature_ids] = self.MASK
-        tokenized_input["input_ids"] = tokens
+        # perturb input by masking the tokens at `top_feature_ids` and evaluate for infidelity
+        infid = self.eval_perturbation(
+            tokenized_input, predicted_class_id, logits, explanation, top_feature_ids)
 
-        # get output of model on perturbed input
-        with torch.no_grad():
-            perturbed_logits = self.model(**tokenized_input).logits
+        return infid
 
-        # calculate local infidelity
-        # compares top_feature importance to change in model output after dropping top_feature
-        infidelity = (sum(top_features) - (logits[0, predicted_class_id] -
-                                           perturbed_logits[0, predicted_class_id]))**2
-
-        return infidelity.item()
-
-    def get_top_k_features(self, explanation, method, predicted_class_id, k=1):
+    def get_top_k_features(self, explanation, predicted_class_id, k=1):
         """
         Gets top k features with greatest contribution to predicted class
 
         params:
             explanation: a feature-attribution explanation of some sample
-            method: explanation method used to produce `explanation`
             predicted_class_id: index of predicted class for sample being explained
             k: number of top features to get
 
         returns:
             top_feature_ids: list of indices of top k features
-            top_features: list of top k feature importances
         """
         if k >= len(explanation) or k < 1:
             raise ValueError(
                 'k must be at least 1 and no greater than the number of features')
 
-        if method == 'SHAP':
+        if self.method == 'SHAP':
             # `explanation` is a 2d array with elts:
             # [contribution to label 0, contribution to label 1]
             top_feature_ids = sorted(
                 range(len(explanation)), key=lambda i: explanation[i][predicted_class_id], reverse=True)[:k]
-            top_features = explanation[top_feature_ids, predicted_class_id]
 
-        elif method == 'LIME':
+        elif self.method == 'LIME':
             # `explanation` is a list [(token, importance),...]
             # negative feature importance -> feature contributes to label 0
             # positive feature importance -> feature contributes to label 1
@@ -123,18 +111,15 @@ class Evaluator():
                 top_feature_ids = sorted(
                     range(len(explanation)), key=lambda i: explanation[i][1], reverse=True)[:k]
 
-            top_features = [explanation[i][1] for i in top_feature_ids]
-
-        elif method == 'IG':
+        elif self.method == 'IG':
             # `explanation` is a list of feature importance scores w.r.t. model prediction
             top_feature_ids = sorted(
                 range(len(explanation)), key=lambda i: explanation[i], reverse=True)[:k]
-            top_features = [explanation[i] for i in top_feature_ids]
 
         else:
-            raise ValueError(f'Explanation method {method} not supported')
+            raise ValueError(f'Explanation method {self.method} not supported')
 
-        return top_feature_ids, top_features
+        return top_feature_ids
 
     def get_local_infidelity_mask_pairs(self, input):
         """
@@ -147,7 +132,80 @@ class Evaluator():
         returns:
             local infidelity
         """
-        pass
+        tokenized_input = self.tokenizer(
+            input, return_tensors="pt").to(self.device)
+
+        with torch.no_grad():
+            logits = self.model(**tokenized_input).logits
+
+        predicted_class_id = logits.argmax().item()
+        explanation = self.explainer.explain(input)
+
+        # Get all distinct pairs of feature ids
+        pairs = list(combinations(range(len(explanation)), 2))
+
+        # Accumulate infidelities of all pair-maskings
+        infid = 0
+        for pair in pairs:
+            infid += self.eval_perturbation(tokenized_input,
+                                            predicted_class_id, logits, explanation, pair)
+
+        return infid
+
+    def eval_perturbation(self, tokenized_input, predicted_class_id, logits, explanation, feature_ids):
+        """
+        Masks features of `tokenized_input` at `feature_ids`.
+        Measures change in output of model on perturbed input w.r.t. the importances of the masked features.
+        Returns this local infidelity.
+
+        params:
+            tokenized_input: tokenized input being explained
+            predicted_class_id: index of predicted class for input being explained
+            logits: output of model on input
+            explanation: a feature-attribution explanation of the tokenized input
+            feature_ids: list of indices of features to be masked
+
+        returns:
+            local infidelity
+        """
+        # store copy of original tokens
+        tokens = tokenized_input["input_ids"].clone()
+
+        print(f'original tokens: {tokenized_input["input_ids"]}')
+        print(f'logits: {logits}')
+        print(f'predicted class id: {predicted_class_id}')
+        print(f'explanation: {explanation}')
+        print(f'masking following feature ids: {feature_ids}')
+
+        # perturb input by masking the tokens at `feature_ids`
+        perturbed_tokens = tokenized_input["input_ids"]
+        perturbed_tokens[0, feature_ids] = self.MASK
+        tokenized_input["input_ids"] = perturbed_tokens
+
+        print(f'perturbed tokens: {tokenized_input["input_ids"]}')
+
+        # get output of model on perturbed input
+        with torch.no_grad():
+            perturbed_logits = self.model(**tokenized_input).logits
+
+        # reset original tokens
+        tokenized_input["input_ids"] = tokens
+
+        # get sum of importances of masked features
+        if self.method == 'SHAP':
+            importance_sum = sum(explanation[feature_ids, predicted_class_id])
+        elif self.method == 'LIME':
+            importance_sum = sum([explanation[i][1] for i in feature_ids])
+        elif self.method == 'IG':
+            importance_sum = sum([explanation[i] for i in feature_ids])
+
+        print(f'importance sum: {importance_sum}')
+        # calculate local infidelity
+        # compares sum of masked feature importances to change in model output after perturbation
+        infidelity = (importance_sum - (logits[0, predicted_class_id] -
+                                        perturbed_logits[0, predicted_class_id]))**2
+
+        return infidelity.item()
 
 
 if __name__ == '__main__':
