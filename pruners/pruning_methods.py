29f2fe109e62527a58150b9ff665093baea1072e
camilobrownpinilla <camilobrownpinilla@gmail.com>
2024-12-02 22:18:17 -0500

    Add l1 structured pruning

--- pruners/pruning_methods.py
+++ pruners/pruning_methods.py
@@ -5,20 +5,9 @@
 import torch.nn.utils.prune as prune
 import torch
 
-from nn_pruning import MovementPruningConfig, ModelPatcher
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-from torchvision.models import resnet18
-import torch_pruning as tp
-
-
-
-from sparseml.pytorch.optim import ScheduledModifierManager
-from sparseml.pytorch.utils import ModuleExporter
-
 from pruners.pruner import Pruner
 
-__all__ =  ['RandUnstructured',' L1Unstructured', 'CustomMask', 'OptimalBrainSurgeon']
+__all__ =  ['RandUnstructured',' L1Unstructured', 'CustomMask', 'L1Structured']
 
 class RandUnstructured(Pruner):
     def __init__(self):
@@ -53,45 +42,9 @@ class CustomMask(Pruner):
         super().remove(params)
 
 
+class L1Structured(Pruner):
 
-
-
-
-# Second  order pruning method
-# SparseML's OptimalBrainSurgeon
-# This pruning methods should be used while finetuning the model (possible while training) not as a standalone method
-# Should be used in conjunction with a training loop
-class OptimalBrainSurgeon(Pruner):
-    def __init__(self):
-        pass
-    # model should be huggingface model, pruning_schedule should be a yaml filename string
-    def prune(self, model, pruning_schedule):
-        manager = ScheduledModifierManager.from_yaml(pruning_schedule) # "pruning_schedule.yaml"
-        manager.initialize(model)  
-
-        for epoch in range(num_epochs):
-            manager.modify(model, epoch=epoch)  # Apply the pruning schedule
-
-
-
-# First order pruning method
-class MovementPruning(Pruner):
-    def __init__(self, model, pruning_config):
-        self.model = model
-        self.pruning_config = MovementPruningConfig(**pruning_config)
-        self.patcher = ModelPatcher(self.model, self.pruning_config)
-
-    def prune(self, params):
-        # Apply the pruning
-        self.patcher.patch()
-
-    
-
-
-# Dep Graph Pruning Interface
-class DepGraphPruning(Pruner):
-    def __init__(self, model, pruning_config):
-       pass
-
-    def prune(self, params):
-        pass
+    def prune(params, ptg):
+        for module, name in params:
+            prune.l1_unstructured(module, name, ptg)
+        super().remove(params)
